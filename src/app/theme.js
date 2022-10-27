f76c5d8b5f4242a0eb214c7cf5d0268953997074
Alexander Schilling Miranda <alexander.schilling.miranda@gmail.com>
2022-10-27 18:38:14 -0300

    feat: add tailwindcss and daisyui

---
+++ src/app/theme.js
@@ -0,0 +1,7 @@
+// https://daisyui.com/docs/themes/
+module.exports = {
+  ...require("daisyui/src/colors/themes")["[data-theme=light]"],
+  primary: "#E02957",
+  secondary: "#fff",
+  neutral: "#1f2937",
+};
