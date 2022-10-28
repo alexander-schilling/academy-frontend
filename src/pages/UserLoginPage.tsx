2760775f2a7d55b44282ae242a9125fe3bab617a
Alexander Schilling Miranda <alexander.schilling.miranda@gmail.com>
2022-10-27 21:31:01 -0300

    feat: add main routes and pages

---
+++ src/pages/UserLoginPage.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const UserLoginPage = () => {
+  return <div>UserLoginPage</div>;
+};
--- src/routes/RoutesComponent.tsx
+++ src/routes/RoutesComponent.tsx
@@ -1,13 +1,25 @@
 import React, { FunctionComponent } from "react";
 import { Route, Routes } from "react-router-dom";
 import { AppLayout } from "../layouts/AppLayout";
+import { CoursePage } from "../pages/CoursePage";
+import { CoursesPage } from "../pages/CoursesPage";
 import { HomePage } from "../pages/HomePage";
+import { UserLoginPage } from "../pages/UserLoginPage";
+import { UserRegisterPage } from "../pages/UserRegisterPage";
 
 export const RoutesComponent: FunctionComponent = () => {
   return (
     <Routes>
       <Route path="/" element={<AppLayout />}>
         <Route index element={<HomePage />} />
+        <Route path="users">
+          <Route path="login" element={<UserLoginPage />} />
+          <Route path="register" element={<UserRegisterPage />} />
+        </Route>
+        <Route path="courses">
+          <Route index element={<CoursesPage />} />
+          <Route path=":courseId" element={<CoursePage />} />
+        </Route>
       </Route>
     </Routes>
   );
