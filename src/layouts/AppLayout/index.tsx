0d19dd70f30798a2408815f2d86b93ce6fb3430e
Alexander Schilling Miranda <alexander.schilling.miranda@gmail.com>
2022-10-27 21:21:03 -0300

    feat(layouts/AppLayout): add Navbar

--- src/layouts/AppLayout/index.tsx
+++ src/layouts/AppLayout/index.tsx
@@ -1,10 +1,17 @@
 import React, { FunctionComponent } from "react";
 import { Outlet } from "react-router-dom";
+import NavbarComponent from "../../components/NavbarComponent";
+
+import "./styles.css";
 
 export const AppLayout: FunctionComponent = () => {
   return (
     <div className="app-layout">
-      <Outlet />
+      <NavbarComponent />
+
+      <main className="content-container">
+        <Outlet />
+      </main>
     </div>
   );
 };
